138b53836dd3d88e03dbb1fe093fd31561f1bffe
vishnu1646 <vishnulance98@gmail.com>
2022-11-25 12:58:31 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddtodoComponent } from './addtodo/addtodo.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewalltodoComponent } from './viewalltodo/viewalltodo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const myRoute :Routes =[
   {
     path:"",
     component: AddtodoComponent
+  },
+  {
+    path:"View",
+    component:ViewalltodoComponent
+  },
+  {
+    path:"Navbar",
+    component:NavbarComponent
   }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddtodoComponent
+    AddtodoComponent,
+    ViewalltodoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
