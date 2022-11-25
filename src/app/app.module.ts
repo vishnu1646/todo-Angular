ee2b8b5f915c3b872735343cb7852d293f05cf25
vishnu1646 <vishnulance98@gmail.com>
2022-11-25 12:29:40 +0530

    Adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddtodoComponent } from './addtodo/addtodo.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute :Routes =[
+  {
+    path:"",
+    component: AddtodoComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddtodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
